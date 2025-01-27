eabc6136a0e39cb8cd463fee10794038d275350c
abhishek <aailabeu@gmail.com>
2025-01-27 09:10:16 +0530

    Initial commit

--- 02Counter/index.js
+++
@@ -1,1 +0,0 @@
-console.log("Development server is running...");
--- 02Counter/src/App.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const App = () => {
-  return <h1>Hello, Vite + React!</h1>;
-};
-
-export default App;
